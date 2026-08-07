190f296192bdaf2435899f6499402f8bf27495ae
Rajan <thakurrajan98674@gmail.com>
2026-08-07 16:02:21 +0530

    changes in setting file

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
+STATICFILES_DIR=[
+    'blog/static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT =BASE_DIR/'media'
 
